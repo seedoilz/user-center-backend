5f4e8df2076de849283125661bc18fb89c07f5c4
seedoilz <seedoilz609@qq.com>
2023-09-07 21:37:16 +0800

    first commit

---
+++ src/main/java/com/seedoilz/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.seedoilz.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.seedoilz.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/seedoilz/usercenter/model/User.java
@@ -0,0 +1,11 @@
+package com.seedoilz.usercenter.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
